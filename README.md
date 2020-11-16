df8bbbcad5b6e2a6c1021fc10fe4997b002fb651
ryanb099 <ryekicks@gmail.com>
2020-11-16 12:48:47 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# workspace-firebase-demo-2avzrb
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/workspace-firebase-demo-2avzrb)
---
+++ js/SaveDisplay.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// save the data
+$(".sampleSurvey input[type='submit']").click(function(e){
+  e.preventDefault();
+})
+
+// update the result in table
